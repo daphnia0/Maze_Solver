0f834b926625a6a8b38dc386867559c504f44281
novak <david.novak@nokia.com>
2024-07-02 17:27:36 +0200

    chapter 5 complete

--- maze_solver.py
+++ maze_solver.py
@@ -6,53 +6,70 @@ class Point:
         self.y = y
 
 class Line:
-    def __init__(self,point1,point2) -> None:
-        self.point1 = point1
-        self.point2 = point2
+    def __init__(self,point_1,point_2) -> None:
+        self.point_1 = point_1
+        self.point_2 = point_2
         
     def draw(self,canvas,fill_color):
-        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y,fill=fill_color, width=2)
+        canvas.create_line(self.point_1.x,self.point_1.y,self.point_2.x,self.point_2.y,fill=fill_color, width=2)
         
+class Cell:
+    has_left_wall = True
+    has_right_wall = True
+    has_top_wall = True
+    has_bottom_wall = True
+    
+    def __init__(self,point_1,point_2,win) -> None:
+        self._win = win
+        self._point_topleft = point_1
+        self._point_bottomright = point_2
+    
+    def draw(self):
+        if (self.has_left_wall):
+            left_line = Line(self._point_topleft,Point(self._point_topleft.x,self._point_bottomright.y))
+            self._win.draw_line(left_line,"black")
+        if (self.has_bottom_wall):
+            bottom_line = Line(Point(self._point_topleft.x,self._point_bottomright.y),self._point_bottomright)
+            self._win.draw_line(bottom_line,"black")
+        if (self.has_right_wall):
+            right_line = Line(Point(self._point_bottomright.x,self._point_topleft.y),self._point_bottomright)
+            self._win.draw_line(right_line,"black")
+        if (self.has_top_wall):
+            top_line = Line(self._point_topleft,Point(self._point_bottomright.x,self._point_topleft.y))
+            self._win.draw_line(top_line,"black")
 
-class Window():
+class Window:
     def __init__(self,width,height):
-        self.rootWidget = Tk()
-        self.rootWidget.title("Maze Solver")
-        self.rootWidget.protocol("WM_DELETE_WINDOW",self.close)
+        self._rootWidget = Tk()
+        self._rootWidget.title("Maze Solver")
+        self._rootWidget.protocol("WM_DELETE_WINDOW",self.close)
         
-        self.canvasWidget = Canvas(self.rootWidget, width=width,height=height)
-        self.canvasWidget.pack()
-        self.running = False
+        self._canvasWidget = Canvas(self._rootWidget, width=width,height=height)
+        self._canvasWidget.pack()
+        self._running = False
         
     def draw_line(self,line,fill_color):
-        line.draw(self.canvasWidget,fill_color)
+        line.draw(self._canvasWidget,fill_color)
         
     def redraw(self):
-        self.rootWidget.update_idletasks()
-        self.rootWidget.update()
+        self._rootWidget.update_idletasks()
+        self._rootWidget.update()
         
     def wait_for_close(self):
-        self.running = True
-        while(self.running):
+        self._running = True
+        while(self._running):
             self.redraw()
 
     def close(self):
-        self.running = False
+        self._running = False
         
         
 def main():
     win = Window(800,600)
-    line1 = Line(Point(10,10),Point(20,20))
-    win.draw_line(line1,"blue")
-    line2 = Line(Point(30,10),Point(40,20))
-    win.draw_line(line2,"black")
-    line3 = Line(Point(200,100),Point(400,500))
-    win.draw_line(line3,"green")
-    line4 = Line(Point(300,300),Point(123,421))
-    win.draw_line(line4,"red")
-    line5 = Line(Point(333,444),Point(232,111))
-    win.draw_line(line5,"orange")
-    
+    cell1 = Cell(Point(100,100),Point(200,200),win)
+    cell1.draw()
+    cell2 = Cell(Point(300,300),Point(400,400),win)
+    cell2.draw()
     win.wait_for_close()
     
 if __name__ == "__main__":
