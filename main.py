f03d5a6bafd9040eb7e47f518311e3448a5af70b
novak <david.novak@nokia.com>
2024-07-03 15:59:52 +0200

    chapter 7

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ from cell import Cell
 
 
 def main():
-    win = Window(800,600)
-    cell1 = Cell(Point(100,100),Point(200,200),win)
-    cell1.has_bottom_wall = False
-    cell1.has_right_wall = False
+    win = Window(820,820)
+    cell1 = Cell(Point(10,10),Point(110,110),win)
     cell1.draw()
-    cell2 = Cell(Point(100,200),Point(200,300),win)
-    cell2.has_top_wall = False
+    cell2 = Cell(Point(110,10),Point(210,110),win)
     cell2.draw()
+    cell3 = Cell(Point(210,10),Point(310,110),win)
+    cell3.draw()
+    cell4 = Cell(Point(310,10),Point(410,110),win)
+    cell4.draw()
+    cell5 = Cell(Point(410,10),Point(510,110),win)
+    cell5.draw()
+    cell6 = Cell(Point(510,10),Point(610,110),win)
+    cell6.draw()
+    cell6 = Cell(Point(610,10),Point(710,110),win)
+    cell6.draw()
+    cell6 = Cell(Point(710,10),Point(810,110),win)
+    cell6.draw()
     cell1.draw_move(cell2)
+    cell2.draw_move(cell3)
+    cell3.draw_move(cell4)
+    cell4.draw_move(cell5)
+    cell5.draw_move(cell6)
     win.wait_for_close()
     
 if __name__ == "__main__":
---
+++ maze.py
@@ -0,0 +1,24 @@
+from cell import Cell
+from graphics import Point
+
+class maze:
+    def __init__(self, point_1, num_rows, num_cols, cell_size, win):
+        self.point_1 = point_1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size = cell_size
+        self.win = win
+    
+    def _create_cells(self):
+        cell_list = []
+        for i in range(self.num_rows):
+            row_list = []
+            for j in range(self.num_cols):
+                current_point_x = self.point_1.x+(self.cell_size*j)
+                current_point_y = self.point_1.y+(self.cell_size*i)
+                current_point_upperleft = Point(current_point_x,current_point_y)
+                current_point_lowerRight = Point(current_point_x+self.cell_size,current_point_y+self.cell_size)
+                current_cell=Cell(current_point_upperleft,current_point_lowerRight)
+                row_list.append(current_cell)
+        
+        
